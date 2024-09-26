9bee40c877a700e2713675da226f8a257e693625
Haythamden22 <haythamdenguiri93@gmail.com>
2024-09-26 10:34:10 +0200

    Update main.c

--- day 2-7/main.c
+++ day 2-7/main.c
@@ -3,13 +3,14 @@
 
 int main()
 {
-   char c;
+      char c;
    printf("enter c");
    scanf("%c",&c);
    if(65<=c && c<=90)
     printf("alphabet majuscule");
-    else
-        printf("n'est pas majuscule");
+
+    else if(66<c<=141 && c<=171<73)
+        printf("n'est un alphabet majuscule");
 
     return 0;
 }
